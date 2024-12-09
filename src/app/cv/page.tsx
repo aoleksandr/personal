1a4571df9da1d50d87a6f5db3618bb8435cd1f38
Oleksandr Akhriutin <oakhriutin@llnw.com>
2024-12-09 16:47:27 +0200

    tbd on cv page

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,7 +1,7 @@
 export default function CV() {
   return (
     <main className="mx-auto container text-lg">
-      <p>My name is Oleksandr. I&apos;m a Senior Software Developer. I have more than 10 years of experience</p>
+      <p>tbd</p>
     </main>
   );
 }
