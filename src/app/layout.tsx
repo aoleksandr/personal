b71fc477e366151cc891c14ba38103ce0bfec37c
Oleksandr Akhriutin <oakhriutin@llnw.com>
2024-12-10 15:29:33 +0200

    update readme, favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Oleksandr Akhriutin",
   description: "Personal website",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>👨‍💻</text></svg>"
+  }
 };
 
 export default function RootLayout({
